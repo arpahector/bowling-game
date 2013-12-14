fb8348123fa603f0acd50bcf701a50043f31afa1
Hector Perez <hecpeare@gmail.com>
2013-12-14 14:14:56 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BowlingGame::Application.config.secret_key_base = 'a995b6b0f858fbe511193de8a2bc6f0bc9bc7323108ac7ea2f63e3974fed2f559c2452a3e0d9522851d2bc6f9444710c1a1724a39f0069ca7873ba1bf95d35ae'

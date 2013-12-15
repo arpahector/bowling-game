4ed59a7f47310cb4327e8546face8cd430c2ab39
Hector Perez <hecpeare@gmail.com>
2013-12-15 13:49:55 +0100

    show error msgs in view

--- app/controllers/balls_controller.rb
+++ app/controllers/balls_controller.rb
@@ -1,16 +1,13 @@
 class BallsController < ApplicationController
   def new
+    @ball ||= Ball.new
     @frame_number = Frame.number
     @score = Ball.last.try(:score) || 0
   end
 
   def create
-    logger.debug params.inspect
-    if Ball.create(params[:ball].permit(:pins))
-      new
-      render action: "new"
-    else
-      #error
-    end
+    @ball = Ball.create(params[:ball].permit(:pins))
+    new
+    render action: "new"
   end
 end
